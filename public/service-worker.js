924bad22cd74d89c7efff51d1306cdbead155929
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-18 19:53:14 +0500

    added service worker(install,activate,fetch)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,55 @@
 'use strict';
 
-// CODELAB: Update cache names any time any of the cached files change.
+// Update cache names any time any of the cached files change
 const CACHE_NAME = 'static-cache-v1';
 
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [];
+// list of files to cache
+const FILES_TO_CACHE = [
+  '/offline.html',
+];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-
+  // Precache static resources 
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-
+  // Remove previous cached data from disk
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
+  // fetch event handler 
+  if (evt.request.mode !== 'navigate') {
+    return;
+  }
 
+  evt.respondWith(
+    fetch(evt.request)
+    .catch(() => {
+      return caches.open(CACHE_NAME)
+        .then((cache) => {
+          return cache.match('offline.html');
+        });
+    })
+  );
 });
