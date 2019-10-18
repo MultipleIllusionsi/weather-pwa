d058fc90a3270d75abbe9f81f109c36a9e404bbe
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-18 23:22:32 +0500

    updated from a basic offline exp to full exp

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -148,8 +148,22 @@ function getForecastFromNetwork(coords) {
  * @return {Object} The weather forecast, if the request fails, return null.
  */
 function getForecastFromCache(coords) {
-  // CODELAB: Add code to get weather forecast from the caches object.
-
+  // code to get weather forecast from the caches object
+  if (!('caches' in window)) {
+    return null;
+  }
+  const url = `${window.location.origin}/forecast/${coords}`;
+  return caches.match(url)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
 }
 
 /**
@@ -183,8 +197,11 @@ function updateData() {
   Object.keys(weatherApp.selectedLocations).forEach((key) => {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
-    // CODELAB: Add code to call getForecastFromCache
-
+    // code to call getForecastFromCache
+    getForecastFromCache(location.geo)
+      .then((forecast) => {
+        renderForecast(card, forecast);
+      });
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
       .then((forecast) => {
